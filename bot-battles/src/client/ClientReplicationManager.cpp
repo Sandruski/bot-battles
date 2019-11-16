13c4da679c9d06ab17287e2ce9878c3c8310e5c5
Sandra Alvarez <sandruskiag@gmail.com>
2019-11-16 10:58:18 +0100

    Read InputStream until 0 bytes left

--- bot-battles/src/client/ClientReplicationManager.cpp
+++ bot-battles/src/client/ClientReplicationManager.cpp
@@ -23,7 +23,7 @@ ClientReplicationManager::~ClientReplicationManager()
 //----------------------------------------------------------------------------------------------------
 void ClientReplicationManager::Read(InputMemoryStream& inputStream) const
 {
-    while (inputStream.GetRemainingBitCount() >= 32) {
+    while (inputStream.GetRemainingBitCount() > 0) {
 
         NetworkID networkID = INVALID_NETWORK_ID;
         inputStream.Read(networkID);
