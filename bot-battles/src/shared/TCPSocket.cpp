2bcf3f82d08f4afd53bf0a043063daae37c1051e
Sandra Alvarez <sandruskiag@gmail.com>
2020-04-02 21:01:56 +0200

    Clients connect with lag

--- bot-battles/src/shared/TCPSocket.cpp
+++ bot-battles/src/shared/TCPSocket.cpp
@@ -111,7 +111,7 @@ bool TCPSocket::Connect(const SocketAddress& socketAddress)
         FD_SET(m_socket, &writeSet);
 
         timeval timeout;
-        timeout.tv_sec = 0;
+        timeout.tv_sec = 1; // TODO: MACRO
         timeout.tv_usec = 0;
         iResult = select(0, nullptr, &writeSet, nullptr, &timeout);
         if (iResult == 0 || iResult == SOCKET_ERROR) {
