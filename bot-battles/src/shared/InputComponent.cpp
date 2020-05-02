cd64a2a6833b08412e8226f3f9b989f67cf45499
Sandra Alvarez <sandruskiag@gmail.com>
2020-05-02 20:13:29 +0200

    Reset values to default

--- bot-battles/src/shared/InputComponent.cpp
+++ bot-battles/src/shared/InputComponent.cpp
@@ -5,7 +5,7 @@
 namespace sand {
 //----------------------------------------------------------------------------------------------------
 InputComponent::InputComponent()
-    : m_maxAcceleration(5.0f)
+    : m_maxAcceleration(300.0f)
     , m_maxAngularAcceleration(90.0f)
     , m_acceleration(0.0f, 0.0f)
     , m_angularAcceleration(0.0f)
